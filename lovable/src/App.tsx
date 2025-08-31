05dd93ece96043fe6ed9999b8eb0aef3ccd648da
gabrieldumkt <gabrielribeiropessoal@gmail.com>
2025-08-31 16:33:37 -0300

    Update App.tsx

--- lovable/src/App.tsx
+++ lovable/src/App.tsx
@@ -1,29 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Inscricao from "./pages/Inscricao";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter } from 'react-router-dom'
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/inscricao" element={<Inscricao />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export function Root() {
+  return (
+    <BrowserRouter basename="/lovable">   {/* 👈 importante! */}
+      {/* suas rotas */}
+    </BrowserRouter>
+  )
+}
